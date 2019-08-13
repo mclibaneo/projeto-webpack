8d70d5897ca5a92d707cf65d4c6d34f37efcb686
Murilo Carvalho <mclibaneo@hotmail.com>
2019-08-13 13:00:31 -0300

    aula 5 completa

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -2,6 +2,7 @@ import { NegociacaoController } from './controllers/NegociacaoController.js';
 import { Negociacao } from './domain/index.js';
 import 'bootstrap/dist/css/bootstrap.css'; //procura direto direto dentro da pasta modules
 import 'bootstrap/dist/css/bootstrap-theme.css'; //npm le o css como um module
+import 'bootstrap/js/modal.js'; //para uso do modal pelo bootstrap
 import '../css/meu-css.css'; //'../' indica para ler na pasta do diretorio raiz
 
 const controller = new NegociacaoController();
@@ -10,12 +11,12 @@ const headers = new Headers();
 headers.set('Content-Type', 'application/json');
 const body = JSON.stringify(negociacao);
 const method = 'POST';
-
+const localHost = "http://localhost:3000/";
 const config = { 
     method,
     headers,
     body 
 };
-localHost = "http://localhost:3000/";
+
 fetch(localHost+'negociacoes', config)
     .then(() => console.log('Dado enviado com sucesso'));
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,12 +2,23 @@ const path = require('path');
 const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin'); //para extrair o css do bundle.js em arquivo separado
 const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'); //para otimizar css
+const webpack = require('webpack');
 
 let plugins = [];
 
 plugins.push(
     new extractTextPlugin("styles.css")
 );
+
+//carrega aqui a biblioteca do jQuery no escopo global da aplicacao
+plugins.push(
+    new webpack.ProvidePlugin(
+        {
+            '$': 'jquery/dist/jquery.js',
+            'jQuery': 'jquery/dist/jquery.js'
+        }
+    )
+)
 
 if(process.env.NODE_ENV == 'production') {
     plugins.push(new babiliPlugin());

8a137d630e6bf89b264848d89b133b2ec457b089
Murilo Carvalho <mclibaneo@hotmail.com>
2019-08-13 15:24:48 -0300

    aula 4 completa

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -1,5 +1,8 @@
 import { NegociacaoController } from './controllers/NegociacaoController.js';
 import { Negociacao } from './domain/index.js';
+import 'bootstrap/dist/css/bootstrap.css'; //procura direto direto dentro da pasta modules
+import 'bootstrap/dist/css/bootstrap-theme.css'; //npm le o css como um module
+import '../css/meu-css.css'; //'../' indica para ler na pasta do diretorio raiz
 
 const controller = new NegociacaoController();
 const negociacao = new Negociacao(new Date(), 1, 200);
@@ -13,6 +16,6 @@ const config = {
     headers,
     body 
 };
-localHost = "http://localhost:3000";
-fetch('http://localhost:3000/negociacoes', config)
+localHost = "http://localhost:3000/";
+fetch(localHost+'negociacoes', config)
     .then(() => console.log('Dado enviado com sucesso'));
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,25 @@
 const path = require('path');
 const babiliPlugin = require('babili-webpack-plugin');
+const extractTextPlugin = require('extract-text-webpack-plugin'); //para extrair o css do bundle.js em arquivo separado
+const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'); //para otimizar css
 
 let plugins = [];
 
+plugins.push(
+    new extractTextPlugin("styles.css")
+);
+
 if(process.env.NODE_ENV == 'production') {
     plugins.push(new babiliPlugin());
+    plugins.push(new optimizeCSSAssetsPlugin({
+        cssProcessor: require('cssnano'),
+        cssProcessorOptions: { 
+            discardComments: {
+                removeAll: true 
+            }
+        },
+        canPrint: true
+     }));    
 }
 
 module.exports = {
@@ -22,7 +37,30 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
-            }
+            },
+            { 
+                test: /\.css$/, 
+                use: extractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            { 
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff' 
+            },
+            { 
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            { 
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'file-loader' 
+            },
+            { 
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=image/svg+xml' 
+            }            
         ]
     },
     plugins
